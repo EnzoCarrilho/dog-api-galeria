2bb0553e00a42bc1485a9fe7ee2ca8316131a8b0
ENZO FELIX CARRILHO <25132408@CFP127ALUNO.LOCAL>
2025-09-16 08:01:49 -0300

    Busca na API e criação de imagens funcionando

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ async function buscarImagens(raca){
 function criarImagem(srcImagem){
     const galeria = document.getElementById('container-galeria')
     const imagem = document.createElement('img')
-    imagem.src = srcImagem.url
+    imagem.src = srcImagem
 
     galeria.appendChild(imagem)
 }
@@ -21,9 +21,8 @@ const botaoBuscar = document.getElementById('botao-buscar')
 botaoBuscar.addEventListener('click', async () => {
     const raca = document.getElementById('input-raca').value
     const arrayImagens = await buscarImagens(raca)
-
     for(let i = 0; i < arrayImagens.length; i++){
-        criarImagem(arrayImagens[i])
+       criarImagem(arrayImagens[i])
     }
 })
 
